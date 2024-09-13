9ce2c4a29c7508a149fd59a6b973ad7f973b7ff1
milka999 <36937930+milka999@users.noreply.github.com>
2024-09-13 21:08:47 +0200

    styling za listinge

--- front/react/components/Listing.jsx
+++ front/react/components/Listing.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable react/prop-types */
 import { useParams } from "react-router-dom";
 import Navbar from "./Navbar";
+// eslint-disable-next-line no-unused-vars
 import Carousel from "./Carousel";
 import Footer from "./Footer";
 import { useEffect, useState } from "react";
@@ -29,31 +30,53 @@ export default function Listing() {
   return (
     <>
       <Navbar />
-      <h1 id="listing-title">{listing.title}</h1>
-      <div className="listing">
+      <div className="flex flex-col m-5">
+        <h1 className="text-2xl">{listing.title}</h1>
         <div className="listing-info">
-          <Carousel />
-          <p>{listing.description}</p>
-          <h2>Osnovne karakteristike</h2>
-          <ul>
-            <li>Lokacija: {listing.location}</li>
-            {/* <li>Broj Soba: {listing.numberOfRooms}</li>
-        <li>Broj Kupatila: {listing.numberOfBathrooms}</li>
-        <li>Kvadratura {listing.size}</li> */}
-            <li>Cijena: {listing.price}</li>
-          </ul>
-          <h2>Kontakt telefon: {listing.contact} </h2>
+          {/*  <Carousel /> */}
+          <img src={listing.img_url} className="mt-5 mb-5"></img>
+          <p className="font-light">{listing.description}</p>
+          <div className="flex flex-col p-4 mt-2">
+            <h2 className="text-lg font-semibold mt-5 mb-2">
+              Osnovne karakteristike
+            </h2>
+            <ul>
+              <li>Lokacija: {listing.location}</li>
+              <li>Broj Soba: {listing.numberOfRooms}</li>
+              <li>Broj Kupatila: {listing.numberOfBathrooms}</li>
+              <li>Kvadratura {listing.size}</li>
+              <li>Cijena: {listing.price}€</li>
+              <li>Kontakt telefon: {listing.contact}</li>
+            </ul>
+          </div>
         </div>
-        <div className="listing-contact">
+        <div className="">
           <form>
-            <div className="listing-contact-container">
+            <div className="border-2 border-gray-500 rounded-md flex flex-col p-4">
               <h3>Pošaljite upit</h3>
-              <input type="text" placeholder="Ime" />
-              <input type="text" placeholder="Prezime" />
+              <input
+                type="text"
+                placeholder="Ime"
+                className="p-3 border-2 border-gray-400 rounded-md m-2 mt"
+              />
+              <input
+                type="text"
+                placeholder="Prezime"
+                className="p-3 border-2 border-gray-400 rounded-md m-2"
+              />
               {/* fali validacija za broj telefona */}
-              <input type="text" placeholder="Broj Telefona" />
-              <textarea></textarea>
-              <button className="btn-blue">Pošalji</button>
+              <input
+                type="text"
+                placeholder="Broj Telefona"
+                className="p-3 border-2 border-gray-400 rounded-md m-2"
+              />
+              <textarea
+                className="p-3 border-2 border-gray-400 rounded-md m-2"
+                placeholder="Dodatne informacije"
+              ></textarea>
+              <button className="bg-green-600 rounded-md text-black p-2 hover:bg-black hover:text-white hover:border-green-600 hover:border-solid hover:border-2 w-1/3 mx-auto m-2">
+                Pošalji
+              </button>
             </div>
           </form>
         </div>
--- front/react/components/ListingSmall.jsx
+++ front/react/components/ListingSmall.jsx
@@ -1,21 +1,34 @@
 /* eslint-disable react/prop-types */
 import { Link } from "react-router-dom";
 export default function ListingSmall({ listing }) {
+  const date = new Date(listing.date_uploaded);
+  const formattedDate = date.toLocaleDateString("de-De");
   return (
-    <div className="listing-container">
-      <img src={listing.imgUrl} alt="stan"></img>
-      <div className="listing-text-container">
-        <div>
+    <div className="border-2 rounded-xl m-2">
+      <img
+        src={listing.img_url}
+        alt={listing.name}
+        className="rounded-xl"
+      ></img>
+      <div className="m-2">
+        <div className="flex flex-row justify-between">
           <Link to={`${listing.id}`}>
-            <h2>{listing.title}</h2>
+            <h2 className="text-lg font-bold m-1">{listing.title}</h2>
           </Link>
-          <p>{listing.price}</p>
+          <p className="m-1">{listing.price} €</p>
         </div>
         <a href="">{listing.location}</a>
-        <p>{listing.description}</p>
+        <p className="m-1 font-light">
+          {listing.description.slice(0, 200)} ...
+        </p>
         <div>
-          <p>Kontakt telefon: {listing.contact}</p>
-          <p>Datum objavljivanja: {listing.date}</p>
+          <p className="m-1 font-semibold">
+            Kontakt telefon: {listing.contact}
+          </p>
+          <div className="flex flex-row justify-between">
+            <p className="m-1 font-semibold">Datum objavljivanja:</p>
+            <p className="m-1">{formattedDate}</p>
+          </div>
         </div>
       </div>
     </div>
