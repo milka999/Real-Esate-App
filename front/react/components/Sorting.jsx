1faa6c9bd11156d817e3e670aef9c5c2f652d773
Milka Dedeic <milka.dedeic@coreit.me>
2024-09-17 15:53:08 +0200

    filteri (front)

--- front/react/components/Sorting.jsx
+++ front/react/components/Sorting.jsx
@@ -1,64 +1,25 @@
-import { useState } from "react";
+import {Link} from "react-router-dom";
 /* eslint-disable react/prop-types */
 export default function Sorting() {
-  const [open, setOpen] = useState(false);
-  const [sortiranje, setSortiranje] = useState("Bez Sortiranja");
-
-  function handleOpen() {
-    setOpen(!open);
-  }
-
-  function handleSortPriceAsc() {
-    //logika za sortiranje - poziv na back
-    setSortiranje("Cijena");
-    setOpen(false);
-  }
-
-  function handleSortSizeAsc() {
-    //logika za sortiranje - poziv na back
-    setSortiranje("Kvadratura");
-    setOpen(false);
-  }
-
-  function handleSortPriceDesc() {
-    //logika za sortiranje - poziv na back
-    setSortiranje("Cijena (opadajuće)");
-    setOpen(false);
-  }
-
-  function handleSortSizeDesc() {
-    //logika za sortiranje - poziv na back
-    setSortiranje("Kvadratura (opadajuće)");
-    setOpen(false);
-  }
-
   return (
     <div>
       <div>
-        <button
-          onClick={handleOpen}
-          className="border-2 border-black p-1 rounded-md hover:text-gray-700"
-        >
-          {sortiranje}
-        </button>
-        {open ? (
-          <ul className="menu">
-            <li className="menu-item">
-              <button onClick={handleSortPriceAsc}>Cijena (rastuće)</button>
-            </li>
-            <li className="menu-item">
-              <button onClick={handleSortPriceDesc}>Cijena (opadajuće)</button>
-            </li>
-            <li className="menu-item">
-              <button onClick={handleSortSizeAsc}>Kvadratura (rastuće)</button>
-            </li>
-            <li className="menu-item">
-              <button onClick={handleSortSizeDesc}>
-                Kvadratura (opadajuće)
-              </button>
-            </li>
-          </ul>
-        ) : null}
+        <select className="p-2 rounded-md border-2 border-black m-2">
+          <option key="Bez Sortiranja" value="Bez Soritranja">
+           Bez Sortiranja
+          </option>
+          <option key="price_asc" value="price_asc"> <Link to="/?sort=price_asc">Cijena</Link>
+          </option>
+          <option key="price_desc" value="price_desc">
+            <Link to="/?sort=price_desc">Cijena (opadajuce)</Link>
+          </option>
+          <option key="size_asc" value="size_asc">
+            <Link to="/?sort=size_asc">Kvadratura</Link>
+          </option>
+          <option key="size_desc" value="size_desc">
+            <Link to="/?sort=size_desc">Kvadratura (opadajuce)</Link>
+          </option>
+        </select>
       </div>
     </div>
   );
